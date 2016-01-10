3433d02b430ed2aba67dd0fbe78b3cfa9f9607be
Tunous <lukus178@gmail.com>
2016-01-10 16:23:12 +0100

    Add GetExactPositionInPixels player extension

--- BotBitsExt.Physics/PhysicsService.cs
+++ BotBitsExt.Physics/PhysicsService.cs
@@ -1,59 +1,58 @@
 ﻿using System;
-
 using BotBits;
 using BotBits.Events;
-
 using EEPhysics;
+using JetBrains.Annotations;
 
 namespace BotBitsExt.Physics
 {
     public sealed class PhysicsService : EventListenerPackage<PhysicsService>, IDisposable
     {
-        /// <summary>
-        /// Gets the physics world.
-        /// </summary>
-        /// <value>The physics world.</value>
-        public PhysicsWorld PhysicsWorld { get; private set; }
-
         public PhysicsService()
         {
             PhysicsWorld = new PhysicsWorld();
         }
 
         /// <summary>
-        /// Sets the physics player when someone joins world.
+        ///     Gets the physics world.
         /// </summary>
-        /// <param name="e">Join event.</param>
-        [EventListener]
-        private void OnJoin(JoinEvent e)
-        {
-            e.Player.SetPhysicsPlayer(PhysicsWorld.Players[e.Player.UserId]);
-        }
-
-        /// <summary>
-        /// Sets the physics player when bot joins world.
-        /// </summary>
-        /// <param name="e">Init event.</param>
-        [EventListener]
-        private void OnInit(InitEvent e)
-        {
-            e.Player.SetPhysicsPlayer(PhysicsWorld.Players[e.Player.UserId]);
-        }
-
-        /// <summary>
-        /// Handles the PlayerIOMessage in physics world.
-        /// </summary>
-        /// <param name="e">PlayerIOMessage event.</param>
-        [EventListener(GlobalPriority.BeforeMost)]
-        private void OnMessage(PlayerIOMessageEvent e)
-        {
-            PhysicsWorld.HandleMessage(e.Message);
-        }
+        /// <value>The physics world.</value>
+        [UsedImplicitly]
+        public PhysicsWorld PhysicsWorld { get; }
 
         void IDisposable.Dispose()
         {
             PhysicsWorld.StopSimulation();
         }
+
+        /// <summary>
+        ///     Sets the physics player when someone joins world.
+        /// </summary>
+        /// <param name="e">Join event.</param>
+        [EventListener]
+        private void On(JoinEvent e)
+        {
+            e.Player.SetPhysicsPlayer(PhysicsWorld.Players[e.Player.UserId]);
+        }
+
+        /// <summary>
+        ///     Sets the physics player when bot joins world.
+        /// </summary>
+        /// <param name="e">Init event.</param>
+        [EventListener]
+        private void On(InitEvent e)
+        {
+            e.Player.SetPhysicsPlayer(PhysicsWorld.Players[e.Player.UserId]);
+        }
+
+        /// <summary>
+        ///     Handles the PlayerIOMessage in physics world.
+        /// </summary>
+        /// <param name="e">PlayerIOMessage event.</param>
+        [EventListener(GlobalPriority.BeforeMost)]
+        private void On(PlayerIOMessageEvent e)
+        {
+            PhysicsWorld.HandleMessage(e.Message);
+        }
     }
 }
-
--- BotBitsExt.Physics/PlayerExtensions.cs
+++ BotBitsExt.Physics/PlayerExtensions.cs
@@ -1,36 +1,39 @@
 ﻿using BotBits;
-
 using EEPhysics;
+using JetBrains.Annotations;
 
 namespace BotBitsExt.Physics
 {
     public static class PlayerExtensions
     {
         /// <summary>
-        /// Gets the physics player.
+        ///     Gets the physics player.
         /// </summary>
         /// <returns>The physics player.</returns>
         /// <param name="player">Player.</param>
+        [UsedImplicitly]
         public static PhysicsPlayer GetPhysicsPlayer(this Player player)
         {
             return player.Get<PhysicsPlayer>("PhysicsPlayer");
         }
 
         /// <summary>
-        /// Sets the physics player.
+        ///     Sets the physics player.
         /// </summary>
         /// <param name="player">Player.</param>
         /// <param name="physicsPlayer">Physics player.</param>
-        public static void SetPhysicsPlayer(this Player player, PhysicsPlayer physicsPlayer)
+        internal static void SetPhysicsPlayer(this Player player, PhysicsPlayer physicsPlayer)
         {
             player.Set("PhysicsPlayer", physicsPlayer);
         }
 
         /// <summary>
-        /// Gets the exact position of the player calculated using EEPhysics.
+        ///     Gets the exact position (in blocks) of the player calculated
+        ///     using EEPhysics.
         /// </summary>
-        /// <returns>The exact position of the player.</returns>
-        /// <param name="player">Player.</param>
+        /// <param name="player">The player.</param>
+        /// <returns>The exact position of the player (in blocks).</returns>
+        [UsedImplicitly]
         public static Point GetExactPosition(this Player player)
         {
             var p = player.GetPhysicsPlayer();
@@ -38,6 +41,18 @@ namespace BotBitsExt.Physics
             var y = WorldUtils.PosToBlock((int) p.Y);
             return new Point(x, y);
         }
+
+        /// <summary>
+        ///     Gets the exact position (in pixels) of the player calculated
+        ///     using EEPhysics.
+        /// </summary>
+        /// <param name="player">The player</param>
+        /// <returns>The exact position of the player (in pixels).</returns>
+        [UsedImplicitly]
+        public static Point GetExactPositionInPixels(this Player player)
+        {
+            var p = player.GetPhysicsPlayer();
+            return new Point((int) p.X, (int) p.Y);
+        }
     }
 }
-
